66a66f390c4530b2392bef6bb98672bad0524d1c
Jahid hasan <Vdjsovaj@gmail.com>
2022-08-15 13:31:47 +0600

    Main Activity update

--- app/src/main/java/dev/jahidhasanco/firebasemvvm/MainActivity.kt
+++ app/src/main/java/dev/jahidhasanco/firebasemvvm/MainActivity.kt
@@ -1,10 +1,12 @@
 package dev.jahidhasanco.firebasemvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import dev.jahidhasanco.firebasemvvm.databinding.ActivityMainBinding
+import dev.jahidhasanco.firebasemvvm.utils.displayToast
 import dev.jahidhasanco.firebasemvvm.viewmodel.AuthViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +15,47 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var authViewModel: AuthViewModel
 
+    var email = ""
+    var password = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        authViewModel = ViewModelProviders.of(this)[AuthViewModel::class.java]
 
+        authViewModel.getUserLiveData().observe(this, Observer {
+
+            if (it != null) {
+                // Navigate to Other Activity
+                this.displayToast("SuccessFully Pre-Logged")
+            }
+        })
+
+        binding.authContainer.loginRegisterLogin.setOnClickListener {
+            with(binding.authContainer) {
+                email = loginRegisterEmail.text.toString()
+                password = loginRegisterPassword.text.toString()
+                if (email.isNotEmpty() && password.isNotEmpty()) {
+                    authViewModel.login(email, password)
+                } else {
+                    this@MainActivity.displayToast("Email and Password Must be Entered.")
+                }
+            }
+        }
+
+        binding.authContainer.loginRegisterRegister.setOnClickListener {
+            with(binding.authContainer) {
+                email = loginRegisterEmail.text.toString()
+                password = loginRegisterPassword.text.toString()
+                if (email.isNotEmpty() && password.isNotEmpty()) {
+                    authViewModel.register(email, password)
+                } else {
+                    this@MainActivity.displayToast("Email and Password Must be Entered.")
+                }
+            }
+        }
 
     }
+
 }
