631665339d132c229ba67b8faf504a8563658a39
Jahid hasan <Vdjsovaj@gmail.com>
2022-08-15 13:03:28 +0600

    Login Ui Create and update

--- app/src/main/java/dev/jahidhasanco/firebasemvvm/MainActivity.kt
+++ app/src/main/java/dev/jahidhasanco/firebasemvvm/MainActivity.kt
@@ -2,10 +2,22 @@ package dev.jahidhasanco.firebasemvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import dev.jahidhasanco.firebasemvvm.databinding.ActivityMainBinding
+import dev.jahidhasanco.firebasemvvm.viewmodel.AuthViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var authViewModel: AuthViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+
     }
 }
