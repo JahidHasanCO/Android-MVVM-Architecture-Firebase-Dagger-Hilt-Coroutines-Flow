004ced0d7354ffe367dc048812d19407cfd14cb6
Jahid hasan <Vdjsovaj@gmail.com>
2022-08-15 12:37:06 +0600

    AuthViewModel.kt Create

--- app/src/main/java/dev/jahidhasanco/firebasemvvm/viewmodel/AuthViewModel.kt
+++ app/src/main/java/dev/jahidhasanco/firebasemvvm/viewmodel/AuthViewModel.kt
@@ -1,13 +1,30 @@
 package dev.jahidhasanco.firebasemvvm.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
 import dev.jahidhasanco.firebasemvvm.repository.AuthRepository
 
 class AuthViewModel constructor(
-    private val application: Application,
-    private val authRepository: AuthRepository
+    application: Application,
+    private var authRepository: AuthRepository
 ) : ViewModel() {
 
+    private var userLiveData: MutableLiveData<FirebaseUser> = MutableLiveData()
 
+    init {
+        authRepository = AuthRepository(application)
+        userLiveData = authRepository.getUserLiveData()
+    }
+
+    fun login(email: String, password: String) {
+        authRepository.login(email, password)
+    }
+
+    fun register(email: String, password: String) {
+        authRepository.register(email, password)
+    }
+
+    fun getUserLiveData() = userLiveData
 }
